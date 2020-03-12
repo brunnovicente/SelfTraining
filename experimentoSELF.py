35a48c7b3ede2e4f024e07979cc418f8d01ea4af
brunnovicente <brunnovicente@gmail.com>
2020-03-12 01:24:22 -0300

    Apenas 1
    Alteração para apenas uma execução

--- experimentoSELF.py
+++ experimentoSELF.py
@@ -15,6 +15,7 @@ from sklearn.linear_model import LogisticRegression
 import time
 from sklearn.utils import shuffle
 from SelfTraining import StandardSelfTraining
+from sklearn.metrics import accuracy_score, cohen_kappa_score
 
 sca = MinMaxScaler()
 base = 'mnist'
@@ -33,102 +34,171 @@ porcentagem = [0.0047, 0.0093, 0.0140, 0.0186, 0.0233, 0.0279]
 rotulados = [50, 100, 150, 200, 250, 300]
 porcentagem = [0.0047, 0.0093, 0.0140, 0.0186, 0.0233, 0.0279]
 
+resultadoMLPi = pd.DataFrame()
+resultadoKNNi = pd.DataFrame()
+resultadoSVMi = pd.DataFrame()
+resultadoRFi = pd.DataFrame()
+resultadoNBi = pd.DataFrame()
+resultadoLRi = pd.DataFrame()
+
+resultadoMLPt = pd.DataFrame()
+resultadoKNNt = pd.DataFrame()
+resultadoSVMt = pd.DataFrame()
+resultadoRFt = pd.DataFrame()
+resultadoNBt = pd.DataFrame()
+resultadoLRt = pd.DataFrame()
+
+acuraciaMLPi = []
+acuraciaKNNi = []
+acuraciaSVMi = []
+acuraciaRFi = []
+acuraciaNBi = []
+acuraciaLRi = []
+
+kappaMLPi = []
+kappaKNNi = []
+kappaSVMi = []
+kappaRFi = []
+kappaNBi = []
+kappaLRi = []
+
+acuraciaMLPt = []
+acuraciaKNNt = []
+acuraciaSVMt = []
+acuraciaRFt = []
+acuraciaNBt = []
+acuraciaLRt = []
+
+kappaMLPt = []
+kappaKNNt = []
+kappaSVMt = []
+kappaRFt = []
+kappaNBt = []
+kappaLRt = []
+
 for r, p in enumerate(porcentagem):
     
-    resultadoMLP = pd.DataFrame()
-    resultadoKNN = pd.DataFrame()
-    resultadoSVM = pd.DataFrame()
-    resultadoRF = pd.DataFrame()
-    resultadoNB = pd.DataFrame()
-    resultadoLR = pd.DataFrame()
     
-    resultadoMLP_T = pd.DataFrame()
-    resultadoKNN_T = pd.DataFrame()
-    resultadoSVM_T = pd.DataFrame()
-    resultadoRF_T = pd.DataFrame()
-    resultadoNB_T = pd.DataFrame()
-    resultadoLR_T = pd.DataFrame()
     inicio = time.time()
     
-    for k in np.arange(10):
-        print('Teste: '+str(rotulados[r])+' - '+str(k+1))
+    
+    print('Teste: '+str(rotulados[r]))
+    
+    X_train, X_test, y_train, y_test = train_test_split(X,Y, train_size=0.9, test_size=0.1, stratify=Y)
+            
+    """ PROCESSO TRANSDUTIVO """
+    L, U, y, yu = train_test_split(X_train, y_train, train_size = p, test_size= 1.0 - p, stratify=y_train)
+          
+    mlp = MLPClassifier(hidden_layer_sizes=(10,), max_iter=100)
+    knn = KNeighborsClassifier(n_neighbors=5)
+    svm = SVC(probability=True)
+    rf = RandomForestClassifier(n_estimators=20)
+    nb = GaussianNB()
+    lr = LogisticRegression()
+    
+    self1 = StandardSelfTraining('MLP', mlp)
+    self2 = StandardSelfTraining('KNN', knn)
+    self3 = StandardSelfTraining('SVM', svm)
+    self4 = StandardSelfTraining('RF', rf)
+    self5 = StandardSelfTraining('NB', nb)
+    self6 = StandardSelfTraining('LR', lr)
+    
+    X_treino = np.concatenate((L, U))
+    Y_treino = np.concatenate((y.astype(str), np.full_like(yu.astype(str), "unlabeled")))
+            
+    self1.fit(X_treino, Y_treino)
+    self2.fit(X_treino, Y_treino)
+    self3.fit(X_treino, Y_treino)
+    self4.fit(X_treino, Y_treino)
+    self5.fit(X_treino, Y_treino)
+    self6.fit(X_treino, Y_treino)
+    
+    """ PROCESSO TRANDUTIVO """
+    acuraciaMLPi.append(accuracy_score(yu, self1.predict(U).astype('int64')))
+    acuraciaKNNi.append(accuracy_score(yu, self2.predict(U).astype('int64')))
+    acuraciaSVMi.append(accuracy_score(yu, self3.predict(U).astype('int64')))
+    acuraciaRFi.append(accuracy_score(yu, self4.predict(U).astype('int64')))
+    acuraciaNBi.append(accuracy_score(yu, self5.predict(U).astype('int64')))
+    acuraciaLRi.append(accuracy_score(yu, self6.predict(U).astype('int64')))
+   
         
-        X_train, X_test, y_train, y_test = train_test_split(X,Y, train_size=0.9, test_size=0.1, stratify=Y)
-                
-        """ PROCESSO TRANSDUTIVO """
-        L, U, y, yu = train_test_split(X_train, y_train, train_size = p, test_size= 1.0 - p, stratify=y_train)
-              
-        mlp = MLPClassifier(hidden_layer_sizes=(10,), max_iter=100)
-        knn = KNeighborsClassifier(n_neighbors=5)
-        svm = SVC(probability=True)
-        rf = RandomForestClassifier(n_estimators=20)
-        nb = GaussianNB()
-        lr = LogisticRegression()
-        
-        self1 = StandardSelfTraining('MLP', mlp)
-        self2 = StandardSelfTraining('KNN', knn)
-        self3 = StandardSelfTraining('SVM', svm)
-        self4 = StandardSelfTraining('RF', rf)
-        self5 = StandardSelfTraining('NB', nb)
-        self6 = StandardSelfTraining('LR', lr)
-        
-        X_treino = np.concatenate((L, U))
-        Y_treino = np.concatenate((    
-            y.astype(str),
-            np.full_like(yu.astype(str), "unlabeled")
-            ))
-                
-        self1.fit(X_treino, Y_treino)
-        self2.fit(X_treino, Y_treino)
-        self3.fit(X_treino, Y_treino)
-        self4.fit(X_treino, Y_treino)
-        self5.fit(X_treino, Y_treino)
-        self6.fit(X_treino, Y_treino)
-        
-        """ PROCESSO TRANDUTIVO """
-        
-        resultadoMLP['exe'+str(k+1)] = self1.predict(U).astype('int64')
-        resultadoMLP['y'+str(k+1)] = yu
-        resultadoKNN['exe'+str(k+1)] = self2.predict(U).astype('int64')
-        resultadoKNN['y'+str(k+1)] = yu
-        resultadoSVM['exe'+str(k+1)] = self3.predict(U).astype('int64')
-        resultadoSVM['y'+str(k+1)] = yu
-        resultadoRF['exe'+str(k+1)] = self4.predict(U).astype('int64')
-        resultadoRF['y'+str(k+1)] = yu
-        resultadoNB['exe'+str(k+1)] = self5.predict(U).astype('int64')
-        resultadoNB['y'+str(k+1)] = yu
-        resultadoLR['exe'+str(k+1)] = self6.predict(U).astype('int64')
-        resultadoLR['y'+str(k+1)] = yu
-        
-        
-        """ PROCESSO INDUTIVO """
-        resultadoMLP_T['exe'+str(k+1)] = self1.predict(X_test).astype('int64')
-        resultadoMLP_T['y'+str(k+1)] = y_test
-        resultadoKNN_T['exe'+str(k+1)] = self2.predict(X_test).astype('int64')
-        resultadoKNN_T['y'+str(k+1)] = y_test
-        resultadoSVM_T['exe'+str(k+1)] = self3.predict(X_test).astype('int64')
-        resultadoSVM_T['y'+str(k+1)] = y_test
-        resultadoRF_T['exe'+str(k+1)] = self4.predict(X_test).astype('int64')
-        resultadoRF_T['y'+str(k+1)] = y_test
-        resultadoNB_T['exe'+str(k+1)] = self5.predict(X_test).astype('int64')
-        resultadoNB_T['y'+str(k+1)] = y_test
-        resultadoLR_T['exe'+str(k+1)] = self6.predict(X_test).astype('int64')
-        resultadoLR_T['y'+str(k+1)] = y_test
+    kappaMLPi.append(cohen_kappa_score(yu, self1.predict(U).astype('int64')))
+    kappaKNNi.append(cohen_kappa_score(yu, self2.predict(U).astype('int64')))
+    kappaSVMi.append(cohen_kappa_score(yu, self3.predict(U).astype('int64')))
+    kappaRFi.append(cohen_kappa_score(yu, self4.predict(U).astype('int64')))
+    kappaNBi.append(cohen_kappa_score(yu, self5.predict(U).astype('int64')))
+    kappaLRi.append(cohen_kappa_score(yu, self6.predict(U).astype('int64')))
+    
+    acuraciaMLPt.append(accuracy_score(y_test, self1.predict(X_test).astype('int64')))
+    acuraciaKNNt.append(accuracy_score(y_test, self2.predict(X_test).astype('int64')))
+    acuraciaSVMt.append(accuracy_score(y_test, self3.predict(X_test).astype('int64')))
+    acuraciaRFt.append(accuracy_score(y_test, self4.predict(X_test).astype('int64')))
+    acuraciaNBt.append(accuracy_score(y_test, self5.predict(X_test).astype('int64')))
+    acuraciaLRt.append(accuracy_score(y_test, self6.predict(X_test).astype('int64')))
+    
+    kappaMLPt.append(cohen_kappa_score(y_test, self1.predict(X_test).astype('int64')))
+    kappaKNNt.append(cohen_kappa_score(y_test, self2.predict(X_test).astype('int64')))
+    kappaSVMt.append(cohen_kappa_score(y_test, self3.predict(X_test).astype('int64')))
+    kappaRFt.append(cohen_kappa_score(y_test, self4.predict(X_test).astype('int64')))
+    kappaNBt.append(cohen_kappa_score(y_test, self5.predict(X_test).astype('int64')))
+    kappaLRt.append(cohen_kappa_score(y_test, self6.predict(X_test).astype('int64')))    
         
     fim = time.time()
     tempo = np.round((fim - inicio)/60,2)
     print('........ Tempo: '+str(tempo)+' minutos.')
-                    
-    resultadoMLP.to_csv('resultados/resultado_SELF_MLP_'+str(rotulados[r])+base+'.csv', index=False)
-    resultadoKNN.to_csv('resultados/resultado_SELF_KNN_'+str(rotulados[r])+base+'.csv', index=False)
-    resultadoSVM.to_csv('resultados/resultado_SELF_SVM_'+str(rotulados[r])+base+'.csv', index=False)
-    resultadoRF.to_csv('resultados/resultado_SELF_RF_'+str(rotulados[r])+base+'.csv', index=False)
-    resultadoNB.to_csv('resultados/resultado_SELF_NB_'+str(rotulados[r])+base+'.csv', index=False)
-    resultadoLR.to_csv('resultados/resultado_SEKF_LR_'+str(rotulados[r])+base+'.csv', index=False)
-    
-    resultadoMLP_T.to_csv('resultados/resultado_SELF_MLP_T'+str(rotulados[r])+base+'.csv', index=False)
-    resultadoKNN_T.to_csv('resultados/resultado_SELF_KNN_T'+str(rotulados[r])+base+'.csv', index=False)
-    resultadoSVM_T.to_csv('resultados/resultado_SELF_SVM_T'+str(rotulados[r])+base+'.csv', index=False)
-    resultadoRF_T.to_csv('resultados/resultado_SELF_RF_T'+str(rotulados[r])+base+'.csv', index=False)
-    resultadoNB_T.to_csv('resultados/resultado_SELF_NB_T'+str(rotulados[r])+base+'.csv', index=False)
-    resultadoLR_T.to_csv('resultados/resultado_SEKF_LR_T'+str(rotulados[r])+base+'.csv', index=False)
+
+resultadoMLPi['R'] = rotulados
+resultadoKNNi['R'] = rotulados
+resultadoSVMi['R'] = rotulados
+resultadoRFi['R'] = rotulados
+resultadoNBi['R'] = rotulados
+resultadoLRi['R'] = rotulados
+
+resultadoMLPt['R'] = rotulados
+resultadoKNNt['R'] = rotulados
+resultadoSVMt['R'] = rotulados
+resultadoRFt['R'] = rotulados
+resultadoNBt['R'] = rotulados
+resultadoLRt['R'] = rotulados
+
+resultadoMLPi['ACURACIA'] = acuraciaMLPi
+resultadoKNNi['ACURACIA'] = acuraciaKNNi
+resultadoSVMi['ACURACIA'] = acuraciaMLPi
+resultadoRFi['ACURACIA'] = acuraciaRFi
+resultadoNBi['ACURACIA'] = acuraciaNBi
+resultadoLRi['ACURACIA'] = acuraciaLRi
+
+resultadoMLPi['KAPPA'] = kappaMLPi
+resultadoKNNi['KAPPA']  = kappaKNNi
+resultadoSVMi['KAPPA']  = kappaSVMi
+resultadoRFi['KAPPA']  = kappaRFi
+resultadoNBi['KAPPA']  = kappaNBi
+resultadoLRi['KAPPA']  = kappaLRi
+
+resultadoMLPt['ACURACIA'] = acuraciaMLPt
+resultadoKNNt['ACURACIA'] = acuraciaKNNt
+resultadoSVMt['ACURACIA'] = acuraciaMLPt
+resultadoRFt['ACURACIA'] = acuraciaRFt
+resultadoNBt['ACURACIA'] = acuraciaNBt
+resultadoLRt['ACURACIA'] = acuraciaLRt
+
+resultadoMLPt['KAPPA'] = kappaMLPt
+resultadoKNNt['KAPPA']  = kappaKNNt
+resultadoSVMt['KAPPA']  = kappaSVMt
+resultadoRFt['KAPPA']  = kappaRFt
+resultadoNBt['KAPPA']  = kappaNBt
+resultadoLRt['KAPPA']  = kappaLRt
+
+resultadoMLPi.to_csv('resultados/resultado_SELF_MLP_i_'+base+'.csv', index=False)
+resultadoKNNi.to_csv('resultados/resultado_SELF_KNN_i_'+base+'.csv', index=False)
+resultadoSVMi.to_csv('resultados/resultado_SELF_SVM_i_'+base+'.csv', index=False)
+resultadoRFi.to_csv('resultados/resultado_SELF_RF_i_'+base+'.csv', index=False)
+resultadoNBi.to_csv('resultados/resultado_SELF_NB_i_'+base+'.csv', index=False)
+resultadoLRi.to_csv('resultados/resultado_SEKF_LR_i_'+base+'.csv', index=False)
+
+resultadoMLPt.to_csv('resultados/resultado_SELF_MLP_t'+base+'.csv', index=False)
+resultadoKNNt.to_csv('resultados/resultado_SELF_KNN_t'+base+'.csv', index=False)
+resultadoSVMt.to_csv('resultados/resultado_SELF_SVM_t'++base+'.csv', index=False)
+resultadoRFt.to_csv('resultados/resultado_SELF_RF_t'+base+'.csv', index=False)
+resultadoNBt.to_csv('resultados/resultado_SELF_NB_t'+base+'.csv', index=False)
+resultadoLRt.to_csv('resultados/resultado_SEKF_LRt_'+base+'.csv', index=False)
